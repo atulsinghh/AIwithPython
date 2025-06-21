62bcc420c3b06d85a51d6729432cff905f9fadcc
atulsinghh <atulsingh.h@gmail.com>
2025-06-21 16:33:44 +0530

    code completed for generating password for users based on validation input

--- Py01/PasswordGenerator.py
+++ Py01/PasswordGenerator.py
@@ -1,1 +1,50 @@
-print("Hello World!")
+import random
+import string
+
+def generate_password(min_length,numbers= True,special_characters= True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    password = ""
+
+    meet_criteria = False
+    has_numeric = False
+    has_special = False
+
+    while not meet_criteria or len(password) < min_length:
+        new_char = random.choice(characters)
+        password += new_char
+
+        if new_char in digits:
+            has_numeric = True  
+        elif new_char in special:
+            has_special = True
+
+        meet_criteria = True
+        if numbers:
+            meet_criteria = has_numeric
+        if special_characters:
+            meet_criteria = meet_criteria and has_special
+
+    return password
+
+min_length = int(input("Enter the minimum length of the password: "))
+has_numbers = input("Should the password contain numbers? (yes/no): ").strip().lower() == 'yes'
+has_special = input("Should the password contain special characters? (yes/no): ").strip().lower() == 'yes'
+
+pwd = generate_password(10,True,False)
+
+print(pwd)
+
+
+   
+
+
+
+
